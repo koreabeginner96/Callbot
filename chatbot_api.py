c8640d79e9238150f83969a24b3aa38da149b233
koreabeginner96 <zx028906@naver.com>
2023-11-10 20:38:51 +0900

    fix : postman 가동시 결과값 이상 발생으로 인코더 기능 추가
        "prediction":
            2927
    수정후:"prediction": "what do you mean?"

--- chatbot_api.py
+++ chatbot_api.py
@@ -1,19 +1,20 @@
 from flask import Flask, request, jsonify
 import joblib
 
-# Flask 애플리케이션과 모델을 로드합니다.
+# Flask 애플리케이션과 모델, 벡터라이저, 레이블 인코더를 로드합니다.
 app = Flask(__name__)
 model = joblib.load('data_model.joblib')
 vectorizer = joblib.load('vectorizer.joblib')
+encoder = joblib.load('encoder.joblib')  # 레이블 인코더 추가
 
-# 예측을 수행하는 API 엔드포인트를 정의합니다.
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.json
     text = data['text']
     text_vectorized = vectorizer.transform([text])
     prediction = model.predict(text_vectorized)
-    return jsonify({'prediction': prediction.tolist()})
+    prediction_label = encoder.inverse_transform(prediction)  # 숫자를 레이블로 변환
+    return jsonify({'prediction': prediction_label[0]})
 
 if __name__ == '__main__':
     app.run(debug=True)
